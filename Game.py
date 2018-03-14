e37576537177ad83b44fd383dafd419ac3f511c1
ArthurS1 <33732772+ArthurS1@users.noreply.github.com>
2018-03-14 22:24:13 +0100

    Commit de test.

--- Game.py
+++ Game.py
@@ -2,37 +2,30 @@ import pygame
 import sys
 from pygame.locals import *
 
-#Affichage de la fenêtre
-window=pygame.display.set_mode((400,800))
-global_info=pygame.display.Info()
+print("\nCeci est une version de developpement et ne dois \nen aucun cas être considéré comme le produit final.\n")
 
+#Affichage de la fenêtre
+window=pygame.display.set_mode((400,550))
+info=pygame.display.Info()
 
 #Chargements
-layer=[]
-try:
-    for i in range(0,2):
-        layer.append(pygame.image.load("assets/menu/Layer"+str(i)+".png"))
-    print("Layers Chargées avec succès")
-except:
-    print("Fichiers de layer.s manquant.s");sys.exit()
-#Fonction de super-résolution
-def SuperResolution(PoX=0,PoY=0):
-    info=pygame.display.Info()
-    PoY=PoY/-1
-    PoX=int(info.current_w/2+PoX)
-    PoY=int(info.current_h/2+PoY)
-    return (PoX,PoY)
+background=[]
+for i in range(0,1):
+    try:
+        print("Chargement de data/Back_"+str(i)+".png")
+        background.append(pygame.image.load("data/Back_"+str(i)+".png"))
+    except:
+        print("Une erreur est survenue lors de la tentative de chargement de la ressource")
+        print("Fermeture du programme...")
+        sys.exit()
 
-#menu_layer_0=pygame.transform.scale(menu_layer_0, (global_info.current_w, global_info.current_h))
-#menu_layer_1=pygame.transform.scale(menu_layer_1, (global_info.current_w, global_info.current_h))
-#menu_layer_2=pygame.transform.scale(menu_layer_2, (global_info.current_w, global_info.current_h))
-
-#Affichage du menu
+#%
 maintain=True
+scrolling=0
+scroll_speed=3
 while maintain:
-    w##ndow.blit(lmenu_layer_0SuperResolution(-global_info.current_w/2,global_info.current_h/2))
-    #w#ndow.blit(menu_layer_1,SuperResolution(-global_info.current_w/2,global_info.current_h/2))
-    #w#ndow.blit(menu_layer_2,SuperResolution(-global_info.current_w/2,global_info.current_h/2))
+    scrolling=scrolling+1*scroll_speed
+    window.blit(background[0],(0,scrolling))
     pygame.display.flip()
     for event in pygame.event.get():
         if event.type==QUIT or event.type==KEYDOWN:
