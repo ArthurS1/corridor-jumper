b762cdb7bea127a68d4c87a9db13f7c653d1b274
ArthurS1 <33732772+ArthurS1@users.noreply.github.com>
2018-03-15 22:07:16 +0100

    Améliore le défilement.
    Optimisation du défilement.
    Défilement continu.

--- Game.py
+++ Game.py
@@ -1,8 +1,8 @@
 import pygame
 import sys
 from pygame.locals import *
-
-print("\nCeci est une version de developpement et ne dois \nen aucun cas être considéré comme le produit final.\n")
+nombre_assets=3
+print("\nCeci est une version de developpement et ne dois \nen aucun cas etre consideree comme le produit final.\n")
 
 #Affichage de la fenêtre
 window=pygame.display.set_mode((400,550))
@@ -10,7 +10,7 @@ info=pygame.display.Info()
 
 #Chargements
 background=[]
-for i in range(0,1):
+for i in range(0,nombre_assets):
     try:
         print("Chargement de data/Back_"+str(i)+".png")
         background.append(pygame.image.load("data/Back_"+str(i)+".png"))
@@ -19,14 +19,27 @@ for i in range(0,1):
         print("Fermeture du programme...")
         sys.exit()
 
+#Fait défiler le fond
+def Background(scroll_speed,scrolling):
+    scrolling=scrolling+1*scroll_speed
+    window.blit(background[0],(0,scrolling))
+    window.blit(background[0],(0,scrolling-400))
+    if scrolling>400:
+        scrolling=0
+    return scrolling
+
+print("\nEn cours...")
+
 #%
 maintain=True
 scrolling=0
-scroll_speed=3
+window.blit(background[1],(0,scrolling+400)) #tile pre scroll
 while maintain:
-    scrolling=scrolling+1*scroll_speed
-    window.blit(background[0],(0,scrolling))
+    scrolling=Background(3,scrolling)
     pygame.display.flip()
     for event in pygame.event.get():
         if event.type==QUIT or event.type==KEYDOWN:
             maintain=False
+
+print("\nArret...")
+sys.exit()
