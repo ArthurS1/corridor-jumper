457d3d31a843a185ca6c0594c3273e3c7b273f43
ArthurS1 <33732772+ArthurS1@users.noreply.github.com>
2018-04-25 20:02:42 +0200

    Parcour Fini
    + test obstacle

--- Game.py
+++ Game.py
@@ -5,7 +5,7 @@ from pygame.locals import *
 #Variables du jeu
 maintain=True #Maintiens le jeu ouvert
 scrolling=0 #A quel point le jeu défile, définit le score
-speed_cte=0.1
+speed=1
 score=0
 
 print("\nCeci est une version de developpement et ne dois \nen aucun cas etre consideree comme le produit final.\n")
@@ -18,7 +18,8 @@ info=pygame.display.Info()
 #   Variables dans lesquelles on stock les images
 background=[]
 player=[]
-
+obstacle=[]
+   
 #   Boucles ou on stock les images dans les variables précédentes
 #       Backgrounds
 for i in range(0,1):
@@ -36,35 +37,40 @@ for i in range(0,1):
     except:
         print("Une erreur est survenue\nFermeture du programme...")
         sys.exit()
+#       Obstacles
+for i in range(0,1):
+    try:
+        print("Chargement de data/Obstacle_"+str(i)+".png")
+        obstacle.append(pygame.image.load("data/Obstacle_"+str(i)+".png"))
+    except:
+        print("Une erreur est survenue\nFermeture du programme...")
+        sys.exit()
 
-#Fait defiler une tile
-def Background(tile_scroll,back_number,reset_pos):
-    window.blit(background[back_number],(0,tile_scroll))
-    if tile_scroll>550:
-        return True
+print("\nEn cours...\n")
+print("Détails:")
+print("Facteur vitesse: "+str(speed))
 
-print("\nEn cours...")
-
-tile_scroll_1=150
-tile_scroll_2=-250
-tile_scroll_3=-650
-
+player_pos=180 #Position du joueur sur l'axe x
 #Boucle du jeu
 while maintain:
-    scrolling=scrolling+speed_cte #Changer la vitesse de défilement ici
-    tile_scroll_1=tile_scroll_1+speed_cte
-    tile_scroll_2=tile_scroll_2+speed_cte
-    tile_scroll_3=tile_scroll_3+speed_cte
-    if Background(tile_scroll_1,0)==True:# Si il arrive en bas et est devenu invisible
-        tile_scroll_1=-250
-    if Background(tile_scroll_2,0)==True:# Si il arrive en bas et est devenu invisible
-        tile_scroll_2=-250
-    if Background(tile_scroll_3,0)==True:# Si il arrive en bas et est devenu invisible
-        tile_scroll_2=-250
-    pygame.display.update()
+    scrolling=scrolling+speed
+    window.blit(background[0],(0,scrolling-9450))
+    window.blit(obstacle[0],(130,scrolling))
+    window.blit(player[0],(player_pos,500))
+    pygame.display.flip()
     for event in pygame.event.get():
+        if event.type==KEYDOWN and event.key==K_RIGHT: #Traite la flèche DROITE
+            if player_pos==230: #Restriction des mouvements
+                print("Deplacement a droite impossible.")
+            else:
+                player_pos=player_pos+50
+        if event.type==KEYDOWN and event.key==K_LEFT: #Traite la flèche GAUCHE
+            if player_pos==130: #Restriction des moucements
+                print("Deplacement a gauche impossible.")
+            else:
+                player_pos=player_pos-50
         if event.type==QUIT: #Traite la fin du programme
             maintain=False
 
 print("\nArret...")
-sys.exit()
+sys.exit() 
